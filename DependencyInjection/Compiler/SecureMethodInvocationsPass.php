9ccc70e15409ad7e7fb6eaf0dfb41cd93db54344
Johannes M. Schmitt <schmittjoh@gmail.com>
2010-12-16 15:47:44 +0100

    doc fixes

--- DependencyInjection/Compiler/SecureMethodInvocationsPass.php
+++ DependencyInjection/Compiler/SecureMethodInvocationsPass.php
@@ -3,37 +3,31 @@
 namespace Bundle\JMS\SecurityExtraBundle\DependencyInjection\Compiler;
 
 use Bundle\JMS\SecurityExtraBundle\Generator\ProxyClassGenerator;
-
 use Bundle\JMS\SecurityExtraBundle\Mapping\Driver\DriverChain;
-use Bundle\JMS\SecurityExtraBundle\Mapping\ClassMetadata;
-use Bundle\JMS\SecurityExtraBundle\Annotation\SecureParam;
-use Symfony\Component\DependencyInjection\Resource\FileResource;
-use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\Security\SecurityContext;
-use Bundle\JMS\SecurityExtraBundle\Annotation\Secure;
-use Symfony\Component\Finder\Finder;
-use Doctrine\Common\Annotations\AnnotationReader;
 use \ReflectionClass;
 use \ReflectionMethod;
-use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Resource\FileResource;
+use Symfony\Component\Security\SecurityContext;
 
 /*
-* Copyright 2010 Johannes M. Schmitt
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2010 Johannes M. Schmitt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /**
  * Modifies the container, and sets the proxy classes where needed
