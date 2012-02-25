54b04e183dd2949841fd0faf085ddb53cf3d5e7d
Johannes M. Schmitt <schmittjoh@gmail.com>
2012-02-25 17:32:56 -0600

    removed compatibility code for older Doctrine versions

--- Annotation/PreAuthorize.php
+++ Annotation/PreAuthorize.php
@@ -35,25 +35,4 @@ final class PreAuthorize
      * @var string
      */
     public $expr;
-
-    public function __construct()
-    {
-        if (0 === func_num_args()) {
-            return;
-        }
-        $values = func_get_arg(0);
-
-        if (isset($values['value'])) {
-            $values['expr'] = $values['value'];
-        }
-        if (!isset($values['expr'])) {
-            throw new InvalidArgumentException('The "expr" attribute must be set for annotation @PreAuthorize.');
-        }
-
-        if (!is_string($values['expr'])) {
-            throw new InvalidArgumentException(sprintf('The "expr" attribute of annotation @PreAuthorize must be a string, but got "%s".', gettype($values['expr'])));
-        }
-
-        $this->expr = $values['expr'];
-    }
 }
