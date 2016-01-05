56096c77c97185ad997b558d354174c368d8fac9
Ener-Getick <egetick@gmail.com>
2016-01-05 22:22:16 +0100

    Skip the String test when using php 7

--- Tests/Security/Util/StringTest.php
+++ Tests/Security/Util/StringTest.php
@@ -8,6 +8,9 @@ class StringTest extends \PHPUnit_Framework_TestCase
 {
     public function testEquals()
     {
+        if(PHP_VERSION_ID > 70000) {
+            return $this->markTestSkipped('String class name can\'t be used on php 7.');
+        }
         $this->assertTrue(String::equals('password', 'password'));
         $this->assertFalse(String::equals('password', 'foo'));
     }
