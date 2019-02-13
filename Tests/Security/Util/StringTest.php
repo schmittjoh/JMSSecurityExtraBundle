fc6f29a012b279af3ead5408bd630804245e62bc
Jamal <jamal@mywheels.nl>
2019-02-13 10:06:21 +0100

    Rename String Class to Text Class

--- Tests/Security/Util/StringTest.php
+++ Tests/Security/Util/StringTest.php
@@ -2,16 +2,13 @@
 
 namespace JMS\SecurityExtraBundle\Tests\Security\Util;
 
-use JMS\SecurityExtraBundle\Security\Util\String as StringUtil;
+use JMS\SecurityExtraBundle\Security\Util\Text as TextUtil;
 
 class StringTest extends \PHPUnit_Framework_TestCase
 {
     public function testEquals()
     {
-        if(PHP_VERSION_ID >= 70000) {
-            return $this->markTestSkipped('String class name can\'t be used on php 7.');
-        }
-        $this->assertTrue(StringUtil::equals('password', 'password'));
-        $this->assertFalse(StringUtil::equals('password', 'foo'));
+        $this->assertTrue(TextUtil::equals('password', 'password'));
+        $this->assertFalse(TextUtil::equals('password', 'foo'));
     }
 }
